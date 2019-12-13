9a9c0074bd2df6a2fe8497340bc4423a68753b7e
greg Delmee <greg.delmee@gmail.com>
2019-12-13 15:57:59 +0100

    add new function to app.js for githubtesting

--- app.js
+++ app.js
@@ -2,3 +2,15 @@ console.log('test')
 function add(){
     console.log("Hey");
 };
+
+add()
+function n(a){
+    a++;
+    if(a===3)
+    {
+        return n(a);
+    }
+        else {
+            return a
+        }
+}
